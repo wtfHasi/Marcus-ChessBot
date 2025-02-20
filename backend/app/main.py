c45dde02b0311c5e3b66acce163858496dc058e4
Hasith Sandesh Vitanage <129611058+wtfHasi@users.noreply.github.com>
2025-02-20 12:46:47 +0530

    update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,18 +1,20 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from app.stockfish import set_position, get_best_move, stockfish  # Ensure stockfish is imported properly
+from app.stockfish import set_position, get_best_move, stockfish
 
-# FastAPI app initialization
 app = FastAPI()
+load_dotenv()
 
-# Add CORS middleware here
+# CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Allow frontend URL (e.g., Vite)
+    allow_origins=[os.getenv("FRONTEND_URL")],
     allow_credentials=True,
-    allow_methods=["*"],  # Allow all HTTP methods like GET, POST, OPTIONS
-    allow_headers=["*"],  # Allow all headers
+    allow_methods=["*"],  
+    allow_headers=["*"], 
 )
 
 # Define your routes below
@@ -22,8 +24,8 @@ class MoveRequest(BaseModel):
 @app.post("/make_move/")
 async def make_move(request: MoveRequest):
     user_move = request.move
-    set_position(user_move)  # Call your backend logic
-    bot_move = get_best_move()  # Get the bot's move
+    set_position(user_move)
+    bot_move = get_best_move()
     return {"bot_move": bot_move}
 
 @app.post("/reset_game/")
@@ -37,4 +39,3 @@ async def reset_game():
     except Exception as e:
         print(f"Error resetting Stockfish: {e}")
         return {"status": "error", "message": str(e)}
-
